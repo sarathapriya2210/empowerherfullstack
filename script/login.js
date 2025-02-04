805623eab15f06af0a85a9eb72799630099f710e
sarathapriya2210 <sarathapriya769@gmail.com>
2025-02-04 15:27:19 +0530

    changes in signup page

---
+++ script/login.js
@@ -0,0 +1,34 @@
+import { baseUrl } from "./baseUrl.js";
+
+let form = document.getElementById("form")
+form.addEventListener("submit",function(){
+    event.preventDefault()
+   
+    let email = form.email.value;
+    let password = form.password.value;
+   
+
+   fetch(`${baseUrl}`)
+    .then((res)=>res.json())
+    .then((data)=>{
+        let user = data.filter((el,i)=>el.email==email)
+        if (user.length != 0){
+            if(user[0].password == password){
+                alert("login sucess...")
+                window.location.href = "todo.html"
+
+            }else{
+                alert("password is wrong")
+            }
+        }
+        else{
+           alert("user not registerd prperly")
+           window.location.href="signup.html"
+        }
+    })
+    .catch((err)=>{
+          console.log(err);
+          alert("something went wrong")
+    })
+
+})
